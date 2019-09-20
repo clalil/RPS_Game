50600df60c70c248c8f898b6a70c563430886f23
clalil <cliljander@gmail.com>
2019-09-20 14:38:33 +0200

    Add new class component, test still failing.

---
+++ src/NewMessageForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+
+class NewMessageForm extends Component {
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default NewMessageForm
