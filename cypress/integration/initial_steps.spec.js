8568ff8ac2cfdd24216b47a6d01dc308158e01ea
clalil <cliljander@gmail.com>
2019-09-21 07:56:13 +0200

    Rewrite first test to be more accurate.

--- cypress/integration/initial_steps.spec.js
+++ cypress/integration/initial_steps.spec.js
@@ -3,6 +3,7 @@ import React from "react"
 describe('The home page', function () {
     it('successfully loads', function() {
         cy.visit('/')
-        cy.contains('Rock, Paper, Scissors')
+        cy.get('h1')
+        .should('contain', 'Rock, Paper, Scissors')
     })
 })
