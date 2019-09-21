f6ca50a1a64123a9aace06869bceb39830f3f670
clalil <cliljander@gmail.com>
2019-09-21 08:24:13 +0200

    Test for user_player class still passing.

--- src/Player.js
+++ src/Player.js
@@ -1,5 +1,11 @@
-import React from "react"
+import React, { Component } from "react"
 
-
+class Player extends Component {
+    render() {
+        return (
+            <div className="user_choice"></div>
+        )
+    }
+}
 
 export default Player
