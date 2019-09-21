0d0decbdbdedd53acbc477d2bbb39c7767e85fa0
clalil <cliljander@gmail.com>
2019-09-21 08:20:17 +0200

    Create new Player component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react'
+import Player from "./Player"
 
 class App extends Component {
   render() {
     return(
       <div>
         <h1>Rock, Paper, Scissors</h1>
-        <div className="user_choice"></div>
+        <Player />
       </div>
     )
   }
