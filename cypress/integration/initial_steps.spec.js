eae7f275161183c3f53c3948d5b08a892d5581bf
clalil <cliljander@gmail.com>
2019-09-21 08:06:58 +0200

    Third spec, passing. Add content div.

--- cypress/integration/initial_steps.spec.js
+++ cypress/integration/initial_steps.spec.js
@@ -3,7 +3,11 @@ import React from "react"
 describe('The home page', function () {
     it('successfully loads', function() {
         cy.visit('/')
+
         cy.get('h1')
-        .should('contain', 'Rock, Paper, Scissors')
+        .should('contain', "Rock, Paper, Scissors")
+
+        cy.get('.user_choice')
+
     })
 })
--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ class App extends Component {
     return(
       <div>
         <h1>Rock, Paper, Scissors</h1>
+        <div className="user_choice"></div>
       </div>
     )
   }
