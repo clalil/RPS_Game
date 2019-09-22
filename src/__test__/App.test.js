b42d13331523d0110b2b0c90517cbbfc580f1261
clalil <cliljander@gmail.com>
2019-09-22 18:17:51 +0200

    Add sad path component spec.

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { mount, shallow } from 'enzyme'
 import App from '../App'
+import { exportAllDeclaration } from '@babel/types'
 
 describe('<App />', () => {
 
@@ -14,6 +15,12 @@ describe('<App />', () => {
         const describedComponent = mount(<App />)
         describedComponent.find('#startGame').simulate("click")
         expect(describedComponent.contains('Computer chose')).toBeTruthy;
+    })
+    
+    it('does not display winner/computer choice before a weapon has been selected', () => {
+        const describedComponent = mount(<App />)
+        describedComponent.find('#startGame').simulate('click')
+        expect(describedComponent.contains('You need to choose a weapon')).toEqual(true)
     })
 
 })
