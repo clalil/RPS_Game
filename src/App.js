e1899d826410014bae85ff23b4120e3903bd5693
clalil <cliljander@gmail.com>
2019-09-22 09:21:57 +0200

    Chaange if statement, all tests passing.

--- src/App.js
+++ src/App.js
@@ -5,32 +5,44 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      choices: 0
+      playerChoice: null
     }
     this.updateChoice = this.updateChoice.bind(this)
   }
 
   updateChoice = (type) => {
-    let choice = this.state.choices
+    let choice = this.state.playerChoice
     if(type === 'rock') {
       choice = 1
     } 
-    if(type === 'paper') {
+    else if(type === 'paper') {
       choice = 2
     } 
-    if(type === 'scissors') {
+    else if(type === 'scissors') {
       choice = 3
     } else {
       choice = 'No choice was made'
     }
-    this.setState({choices: choice})
+    debugger;
+    this.setState({playerChoice: choice})
   }
 
   render() {
+
+    let playerChoiceDisplay
+    if(this.state.playerChoice == 1) {
+      playerChoiceDisplay = 'You chose "Rock"'
+    } else if(this.state.playerChoice == 2) {
+      playerChoiceDisplay = 'You chose Paper'
+    } else {
+      playerChoiceDisplay = ' You chose Scissors'
+    }
+
     return(
       <div>
         <h1>Rock, Paper, Scissors</h1>
         <Player onClick={this.updateChoice} />
+        {playerChoiceDisplay}
       </div>
     )
   }
