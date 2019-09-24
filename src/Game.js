26b4001a42e9c6cff9e9186f9d1c74a6bf87e3fa
clalil <cliljander@gmail.com>
2019-09-24 19:32:17 +0200

    Refactor code to avoid DRY.

--- src/Game.js
+++ src/Game.js
@@ -1,96 +1,47 @@
 import React, {Component} from 'react'
 import Player from "./Player"
-import Computer from './Computer'
+
+const baseState = {
+  gameResult: null
+}
 
 class Game extends Component {
     constructor(props) {
       super(props)
-      this.state = {
-        playerChoice: null,
-        computerChoice: null,
-        gameResult: null
-      }
-      this.updatePlayerChoice = this.updatePlayerChoice.bind(this)
-      this.updateComputerChoice = this.updateComputerChoice.bind(this)
+      this.state = baseState
     }
   
     resetGame = () => {
-      this.setState({playerChoice: null})
-      this.setState({computerChoice: null})
       this.setState({gameResult: null})
     }
   
-    updatePlayerChoice = (type) => {
-      let playchoice = this.state.playerChoice
-      if(type === 'rock') {
-        playchoice = 1
-      } 
-      else if(type === 'paper') {
-        playchoice = 2
-      } 
-      else if(type === 'scissors') {
-        playchoice = 3
-      } else {
-        playchoice = 'No choice was made'
-      }
-      this.setState({playerChoice: playchoice})
-    }
-  
-    updateComputerChoice = (type) => {
-      let compChoice = this.state.computerChoice
-      compChoice = Math.floor(Math.random() * type.length + 1);
-      this.setState({computerChoice: compChoice})
-      this.gameEndResult(compChoice)
-    }
-  
-    gameEndResult = (compChoice) => {
-      let playerSelection = this.state.playerChoice
-      let compSelection = compChoice
+    updatePlayerChoice = (choice) => {
+      let playerChoice = choice
+      const weaponsArray = ['rock', 'paper', 'scissors']
+      const randomIndex = Math.floor(Math.random() * weaponsArray.length)
+      const randomComputerResult = weaponsArray[randomIndex]
       let gameFinal = this.state.gameResult
-      if(playerSelection === null) {
-        // this.resetGame()
-      } else if(playerSelection === compSelection) {
-        gameFinal = 'It\'s a draw!'
-      } else if((playerSelection === 1 && compSelection === 2) || (playerSelection === 2 && compSelection === 3) || (playerSelection === 3 && compSelection === 1)) {
-        gameFinal = 'Computer won!'
-      } else {
-        gameFinal = 'You won!'
+
+      if (playerChoice === randomComputerResult) {
+        gameFinal = `Player chose ${playerChoice} and Computer chose ${randomComputerResult} - It's a draw!`
+      } else if ((playerChoice === 'rock' && randomComputerResult === 'paper') || (playerChoice === 'paper' && randomComputerResult === 'scissors') || (playerChoice === 'scissors' && randomComputerResult === 'rock')) {
+        gameFinal = `Player chose ${playerChoice} and Computer chose ${randomComputerResult} - Computer won!`
+      } else if ((playerChoice === 'rock' && randomComputerResult === 'scissors') || (playerChoice === 'paper' && randomComputerResult === 'rock') || (playerChoice === 'scissors' && randomComputerResult === 'paper')) {
+        gameFinal = `Player chose ${playerChoice} and Computer chose ${randomComputerResult} - You won!`
       }
      this.setState({gameResult: gameFinal})
     }
   
     render() {
   
-      let playerChoiceDisplay = this.state.playerChoice
-      if(this.state.playerChoice === 1) {
-        playerChoiceDisplay = 'You chose Rock'
-      } else if(this.state.playerChoice === 2) {
-        playerChoiceDisplay = 'You chose Paper'
-      } else if(this.state.playerChoice === 3){
-        playerChoiceDisplay = 'You chose Scissors'
-      }
-  
-      let computerChoiceDisplay
-      if(this.state.computerChoice === 1) {
-        computerChoiceDisplay = 'Computer chose Rock'
-      } else if(this.state.computerChoice === 2) {
-        computerChoiceDisplay = 'Computer chose Paper'
-      } else if(this.state.computerChoice === 3){
-        computerChoiceDisplay = 'Computer chose Scissors'
-      }
-  
       let whoWon = this.state.gameResult
   
       return(
         <div className=".container">
           <h1>Rock, Paper, Scissors</h1>
-          <Player onClick={this.updatePlayerChoice} />
-          {playerChoiceDisplay}
-          <Computer 
-          onClick={this.updateComputerChoice} />
-          <div className="chompChoice">
-          {computerChoiceDisplay}
-          </div>
+          <Player 
+          onClick={this.updatePlayerChoice}
+           />
           <button onClick={this.resetGame}>Reset Game</button>
           <div>
           {whoWon}
