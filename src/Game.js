8602a05a6438d282f6574ff81fad486fc1ce7246
clalil <cliljander@gmail.com>
2019-09-25 06:48:55 +0200

    Add scoreboard to Game component.

--- src/Game.js
+++ src/Game.js
@@ -2,7 +2,9 @@ import React, {Component} from 'react'
 import Player from "./Player"
 
 const baseState = {
-  gameResult: null
+  gameResult: null,
+  opponentScore: 0,
+  playerScore: 0
 }
 
 class Game extends Component {
@@ -15,19 +17,22 @@ class Game extends Component {
       this.setState({gameResult: null})
     }
   
-    updatePlayerChoice = (choice) => {
-      let playerChoice = choice
+    updatePlayerChoice = (playerChoice) => {
       const weaponsArray = ['rock', 'paper', 'scissors']
       const randomIndex = Math.floor(Math.random() * weaponsArray.length)
       const randomComputerResult = weaponsArray[randomIndex]
       let gameFinal = this.state.gameResult
+      let opponentScore = this.state.opponentScore
+      let playerScore = this.state.playerScore
 
       if (playerChoice === randomComputerResult) {
         gameFinal = `Player chose ${playerChoice} and Computer chose ${randomComputerResult} - It's a draw!`
       } else if ((playerChoice === 'rock' && randomComputerResult === 'paper') || (playerChoice === 'paper' && randomComputerResult === 'scissors') || (playerChoice === 'scissors' && randomComputerResult === 'rock')) {
         gameFinal = `Player chose ${playerChoice} and Computer chose ${randomComputerResult} - Computer won!`
+        this.setState({opponentScore: opponentScore + 1})
       } else if ((playerChoice === 'rock' && randomComputerResult === 'scissors') || (playerChoice === 'paper' && randomComputerResult === 'rock') || (playerChoice === 'scissors' && randomComputerResult === 'paper')) {
         gameFinal = `Player chose ${playerChoice} and Computer chose ${randomComputerResult} - You won!`
+        this.setState({playerScore: playerScore + 1})
       }
      this.setState({gameResult: gameFinal});
     }
@@ -35,6 +40,8 @@ class Game extends Component {
     render() {
   
       let whoWon = this.state.gameResult
+      let playerScoreBoard = this.state.playerScore
+      let opponentScoreBoard = this.state.opponentScore
   
       return(
         <div className=".container">
@@ -45,6 +52,8 @@ class Game extends Component {
           <button onClick={this.resetGame}>Try again</button>
           <div>
           {whoWon}
+          {playerScoreBoard}
+          {opponentScoreBoard}
           </div>
         </div>
       )
