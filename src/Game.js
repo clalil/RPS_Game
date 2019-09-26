4f2558158dbd224d306500ca1011866b0a48d08b
clalil <cliljander@gmail.com>
2019-09-26 20:12:57 +0200

    Remove hard coaded values in if/else statement.

--- src/Game.js
+++ src/Game.js
@@ -36,17 +36,18 @@ class Game extends Component {
       updatePlayerChoice = (playerChoice) => {
       const randomIndex = Math.floor(Math.random() * weaponsArray.length)
       const randomComputerResult = weaponsArray[randomIndex]
-
+        console.log(playerChoice)
+        console.log(randomComputerResult)
       let gameFinal = this.state.gameResult
       let opponentScore = this.state.opponentScore
       let playerScore = this.state.playerScore
 
       if (playerChoice === randomComputerResult) {
         gameFinal = `Player chose ${playerChoice} and Computer chose ${randomComputerResult} - It's a draw!`
-      } else if ((playerChoice === 'rock' && randomComputerResult === 'paper') || (playerChoice === 'paper' && randomComputerResult === 'scissors') || (playerChoice === 'scissors' && randomComputerResult === 'rock')) {
+      } else if ((playerChoice === weaponsArray[0] && randomComputerResult === weaponsArray[1]) || (playerChoice === weaponsArray[1] && randomComputerResult === weaponsArray[2]) || (playerChoice === weaponsArray[2] && randomComputerResult === weaponsArray[0])) {
         gameFinal = `Player chose ${playerChoice} and Computer chose ${randomComputerResult} - Computer won!`
         this.setState({opponentScore: opponentScore + 1})
-      } else if ((playerChoice === 'rock' && randomComputerResult === 'scissors') || (playerChoice === 'paper' && randomComputerResult === 'rock') || (playerChoice === 'scissors' && randomComputerResult === 'paper')) {
+      } else if ((playerChoice === weaponsArray[0] && randomComputerResult === weaponsArray[2]) || (playerChoice === weaponsArray[1] && randomComputerResult === weaponsArray[0]) || (playerChoice === weaponsArray[2] && randomComputerResult === weaponsArray[1])) {
         gameFinal = `Player chose ${playerChoice} and Computer chose ${randomComputerResult} - You won!`
         this.setState({playerScore: playerScore + 1})
       }
