b1d49cccad5543de9880a413541c2bb0dd0c15a5
clalil <cliljander@gmail.com>
2019-09-27 06:37:30 +0200

    Add inline style for whoWon to be visible only once game has been played.

--- src/GameResultDisplayed.js
+++ src/GameResultDisplayed.js
@@ -3,8 +3,8 @@ import React from 'react'
 const GameResultDisplayed = (props) => {
         return (
             <div className="gameResults">
-                <div className="ui center aligned segment">
-                <p>{props.whoDidWin}</p>
+                <div className="whoWonDiv">
+                <p style={{backgroundColor: "rgba(0, 0, 0, 0.1)"}}>{props.whoDidWin}</p>
                 </div>
                 <div className="scoreBoard">
                 <p>{props.playerCurrentScore}</p>
