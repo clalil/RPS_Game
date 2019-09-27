a811d04e9344cb204e01995df07f193ba9f003fc
clalil <cliljander@gmail.com>
2019-09-27 07:06:50 +0200

    Add additional styling for games score board.

--- src/GameResultDisplayed.js
+++ src/GameResultDisplayed.js
@@ -2,14 +2,31 @@ import React from 'react'
 
 const GameResultDisplayed = (props) => {
         return (
-            <div className="gameResults">
-                <div className="whoWonDiv">
-                <p style={{backgroundColor: "rgba(0, 0, 0, 0.1)"}}>{props.whoDidWin}</p>
-                </div>
-                <div className="scoreBoard">
-                <p>{props.playerCurrentScore}</p>
-                <p>{props.opponentCurrentScore}</p>
-                </div>
+                <div className="ui raised segments">
+                    <div className="whoWonDiv">
+                        <p style={{padding: "1rem"}}>{props.whoDidWin}</p>
+                    </div>
+
+                    <div className="scoreBoard">
+                            <div className="ui label" tabindex="0">
+                            <div className="ui label">
+                                <i className="user icon"></i> 
+                                You
+                            </div>
+                                <a className="ui basic label">
+                                    <p>{props.playerCurrentScore}</p>
+                                </a>
+                            </div>
+                    <div className="ui label" tabindex="0">
+                        <div className="ui label">
+                            <i className="user secret icon"></i> 
+                            Opponent
+                        </div>
+                            <a className="ui basic label">
+                                <p>{props.opponentCurrentScore}</p>
+                            </a>
+                        </div>
+                    </div>
             </div>
         )
 }
