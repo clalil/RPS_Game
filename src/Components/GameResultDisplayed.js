8c2103c12d9a3ef78af837711d1bcc1f4f4f6348
clalil <cliljander@gmail.com>
2019-10-02 09:44:22 +0200

    Cleaning up code and apply same naming convention for each element.

--- src/Components/GameResultDisplayed.js
+++ src/Components/GameResultDisplayed.js
@@ -2,32 +2,31 @@ import React from "react";
 
 const GameResultDisplayed = props => {
   return (
-    <div className="ui raised segments" id="cyDivResult">
-      <div className="who-won-div">
-        <p style={{ padding: "1rem" }}>{props.whoDidWin}</p>
+  <div className="ui raised segments" id="cyDivResult">
+    <div className="who-won-div">
+      <p style={{ padding: "1rem" }}>{props.whoDidWin}</p>
+    </div>
+    <div className="score-board">
+      <div className="ui label">
+        <div className="ui label">
+          <i className="user icon"></i>
+          You
+        </div>
+        <span className="ui basic label">
+          <p>{props.playerCurrentScore}</p>
+        </span>
       </div>
-
-      <div className="score-board">
+      <div className="ui label">
         <div className="ui label">
-          <div className="ui label">
-            <i className="user icon"></i>
-            You
-          </div>
-          <span className="ui basic label">
-            <p>{props.playerCurrentScore}</p>
-          </span>
+          <i className="user secret icon"></i>
+          Opponent
         </div>
-        <div className="ui label">
-          <div className="ui label">
-            <i className="user secret icon"></i>
-            Opponent
-          </div>
-          <span className="ui basic label">
-            <p>{props.opponentCurrentScore}</p>
-          </span>
-        </div>
+        <span className="ui basic label">
+          <p>{props.opponentCurrentScore}</p>
+        </span>
       </div>
     </div>
+  </div>
   );
 };
 
