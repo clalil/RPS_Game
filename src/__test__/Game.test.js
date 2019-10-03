b6914c1a31cb1aff3a0cb932931e8d74ab69a3d8
clalil <cliljander@gmail.com>
2019-10-03 06:32:19 +0200

    Add before each and after to game specs. Fix indentation.

--- src/__test__/Game.test.js
+++ src/__test__/Game.test.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Game from '../Components/Game'
 import { mount } from 'enzyme'
-import sinon from 'sinon'
 
 describe('<Game />', () => {
+  let describedComponent
+  beforeEach(() => {
+    describedComponent = mount(<Game />);
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  })
 
   it('resets the score board on click', () => {
-    const describedComponent = mount(<Game />);
     describedComponent.setState({ playerScore: 1 });
     describedComponent.setState({ opponentScore: 2 });
     describedComponent.find('#reset-btn').simulate('click');
@@ -15,27 +21,21 @@ describe('<Game />', () => {
   });
 
   it('shows computer as the winner if scissors beats rock', () => {
-      sinon.stub(Math, 'random').returns(0.66)
-      const describedComponent = mount(<Game />);
-      describedComponent.find('#rock-btn').simulate('click')
-      expect(describedComponent.state().gameResult).toEqual('You chose rock and Computer chose paper - Computer wins!')
-      sinon.restore();
+    sinon.stub(Math, 'random').returns(0.66)
+    describedComponent.find('#rock-btn').simulate('click')
+    expect(describedComponent.state().gameResult).toEqual('You chose rock and Computer chose paper - Computer wins!')
   });
 
   it('shows a tied result if both player and computer choose rock', () => {
     sinon.stub(Math, 'random').returns(0.33)
-    const describedComponent = mount(<Game />);
     describedComponent.find('#rock-btn').simulate('click')
     expect(describedComponent.state().gameResult).toEqual("You chose rock and Computer chose rock - It\'s a tie!")
-    sinon.restore();
   });
 
   it('shows player as the winner if rock beats scissors', () => {
     sinon.stub(Math, 'random').returns(0.99)
-    const describedComponent = mount(<Game />);
     describedComponent.find('#rock-btn').simulate('click')
     expect(describedComponent.state().gameResult).toEqual("You chose rock and Computer chose scissors - You win!")
-    sinon.restore();
   });
 
 })
