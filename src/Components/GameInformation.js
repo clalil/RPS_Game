c80491442bc5de025af011b33b3b4e464c7747c2
clalil <cliljander@gmail.com>
2019-10-10 12:35:15 +0200

    Refactors code for GI component.

--- src/Components/GameInformation.js
+++ src/Components/GameInformation.js
@@ -6,21 +6,17 @@ const GameInformation = () => {
       <h1>Rock, Paper, Scissors</h1>
       <h3>Welcome, the rules are simple:</h3>
       <ul>
-        <li>
-          <span style={{ fontWeight: "700" }}>ROCK</span>
-          <span style={{ padding: "0.4rem" }}>beats</span>
-          <span style={{ fontWeight: "700" }}>SCISSORS</span>
-        </li>
-        <li>
-          <span style={{ fontWeight: "700" }}>PAPER</span>
-          <span style={{ padding: "0.4rem" }}>beats</span>
-          <span style={{ fontWeight: "700" }}>ROCK</span>
-        </li>
-        <li>
-          <span style={{ fontWeight: "700" }}>SCISSORS</span>
-          <span style={{ padding: "0.4rem" }}>beats</span>
-          <span style={{ fontWeight: "700" }}>PAPER</span>
-        </li>
+        {[["ROCK", "SCISSORS"],["PAPER", "ROCK"],["SCISSORS", "PAPER"]].map(
+          arr => {
+            return (
+            <li>
+              <span style={{ fontWeight: "700" }}>{arr[0]}</span>
+              <span style={{ padding: "0.4rem" }}>beats</span>
+              <span style={{ fontWeight: "700" }}>{arr[1]}</span>
+            </li>
+            )
+          }
+        )}
       </ul>
     </div>
   );
